c902e1e38e4080b0430bea7912c41c658d9a3706
Ian Balijawa <ianbalijawa16@gmail.com>
2021-09-16 23:05:29 +0300

    added the startup configurations in the index.js  entry point of the server

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const config = require('config');
 const winston = require('winston');
 const app = express();
 
+// startup configurations
+require('./startup/logging');
+require('./startup/cors');
+require('./startup/db');
+require('./startup/validation');
+require('./startup/routes');
+require('./startup/validation');
+
 const PORT = process.env.PORT || config.get('port');
 const server = app.listen(PORT, function () {
 	winston.info(`Server up and running on port: ${PORT}`);
